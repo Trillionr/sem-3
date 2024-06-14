7371105bd335a0afb4ac8458d8116e81a13bf112
Bhimani Maulik <140812702+gituhihi@users.noreply.github.com>
2024-06-14 18:53:16 +0530

    lab1.sql

--- DBMS/lab/lab1.sql
+++ DBMS/lab/lab1.sql
@@ -5,7 +5,11 @@ create table Deposite(
 ,CNAME VARCHAR(50)
 ,BNAME VARCHAR(50)
 ,AMOUNT DECIMAL(6,2)
-,ADATE DATETIME)insert into Deposite values
+,ADATE DATETIME
+)
+
+
+insert into Deposite values
 (101,'ANIL','VRCE',1000.00,'1990-03-01'),
 (102,'SUNIL','AJNI',5000.00,'1996-01-04'),
 (103,'MEHUL','KAROLBAGH',3500.00,'1995-11-17'),
@@ -15,15 +19,54 @@ create table Deposite(
 (107,'SHIVANI','VIRAR',1000.00 ,'1995-09-05'),
 (108,'KRANTI','NEHRU PLACE',5000.00 ,'1995-07-02'),
 (109,'MINU','POWAI',7000.00 ,'1995-08-10');
-create table Branch(
+
+
+create table Branch(
 BNAME VARCHAR(50),
-CITY VARCHAR(50))insert into Branch values('VRCE','NAGPUR'),('AJNI','NAGPUR'),('KAROLBAGH','DELHI'),('CHANDI','DELHI'),('DHARAMPETH','NAGPUR'),('M.G. ROAD','BANGLORE'),('ANDHERI','BOMBAY'),('VIRAR',' BOMBAY'),('NEHRU PLACE','DELHI'),('POWAI','BOMBAY')create table Customers(
+CITY VARCHAR(50)
+)
+
+insert into Branch values
+('VRCE','NAGPUR'),
+('AJNI','NAGPUR'),
+('KAROLBAGH','DELHI'),
+('CHANDI','DELHI'),
+('DHARAMPETH','NAGPUR'),
+('M.G. ROAD','BANGLORE'),
+('ANDHERI','BOMBAY'),
+('VIRAR',' BOMBAY'),
+('NEHRU PLACE','DELHI'),
+('POWAI','BOMBAY')
+
+
+create table Customers(
 CNAME VARCHAR(50),
-CITY VARCHAR(50))insert into Customers values('ANIL ','CALCUTTA'),('SUNIL','DELHI'),('MEHUL','BARODA'),('MANDAR','PATNA'),('MADHURI','NAGPUR'),('PRAMOD','NAGPUR'),('SANDIP','SURAT'),('SHIVANI',' BOMBAY'),('KRANIT','BOMBAY'),('NAREN','BOMBAY')create table Borrow(
+CITY VARCHAR(50)
+)
+
+insert into Customers values
+('ANIL ','CALCUTTA'),
+('SUNIL','DELHI'),
+('MEHUL','BARODA'),
+('MANDAR','PATNA'),
+('MADHURI','NAGPUR'),
+('PRAMOD','NAGPUR'),
+('SANDIP','SURAT'),
+('SHIVANI',' BOMBAY'),
+('KRANIT','BOMBAY'),
+('NAREN','BOMBAY')
+
+
+
+create table Borrow(
 LOANNO INT,
 CNAME VARCHAR(50),
 BNAME VARCHAR(50),
-AMOUNT DECIMAL(8,2))insert into Borrow values
+AMOUNT DECIMAL(8,2)
+)
+
+
+insert into Borrow values
 (201,'ANIL','VRCE',1000.00),
 (206,'MEHUL','AJNI',5000.00),
 (311,'SUNIL','DHARAMPETH',3000.00),
