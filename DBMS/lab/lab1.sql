523ffa3b9f1beac080bfeb3e19185dcb69e3857c
Bhimani Maulik <140812702+gituhihi@users.noreply.github.com>
2024-07-05 09:19:28 +0530

    lab1.sql

--- DBMS/lab/lab1.sql
+++ DBMS/lab/lab1.sql
@@ -315,3 +315,98 @@ select * from Employee
 7)	UPDATE Deposite
 	SET ADATE = 'NULL', BNAME = 'ANDHERI'
 	WHERE ACTNO = 110;
+
+	--lab -4 alter-------------------------
+	--part-A-------------------------------
+
+--1) Add two more columns City VARCHAR (20) and Pincode INT
+	alter table Deposite
+	add city varchar(50),
+	pincode INT;
+
+--2. Change the size of CNAME column from VARCHAR (50) to VARCHAR (35).
+	alter table Deposite 
+	alter column CNAME varchar(35);
+
+--3. Change the data type DECIMAL to INT in amount Column
+	alter table Deposite
+	alter column AMOUNT INT;
+
+	
+	select *from Deposite
+--4. Rename Column ActNo to ANO.
+	SP_RENAME 'Deposite.ACTNO', ANO;
+
+
+--5. Delete Column City from the DEPOSIT table.
+	alter table Deposite
+	drop column City
+
+--6. Change name of table DEPOSIT to DEPOSIT_DETAIL.
+	sp_rename Deposite,DEPOSITE_DETAIL
+
+	select *from DEPOSITE_DETAIL
+
+--part-b------------
+--1. Rename Column ADATE to AOPENDATE OF DEPOSIT_DETAIL table.
+	SP_RENAME 'DEPOSIT_DETAIL.ADATE',AOPENDATE;
+
+--2. Delete Column AOPENDATE from the DEPOSIT_DETAIL table.
+	alter table DEPOSIT_DETAIL
+	drop column AOPENDATE
+--3. Rename Column CNAME to CustomerName.
+	sp_rename 'DEPOSIT_DETAIL.CNAME',CustomerName;
+
+--part-c-------------------------------------------------
+create table STUDENT_DETAIL(
+Enrollment_No VARCHAR(20),
+Name VARCHAR(25),
+CPI DECIMAL(5,2),
+Birthdate DATETIME)
+select *from STUDENT_DETAIL
+
+--	1. Add two more columns City VARCHAR (20) (Not null) and Backlog INT (Null).
+
+alter table STUDENT_DETAIL
+ADD City VARCHAR (20),
+	Backlog INT;
+--2. Change the size of NAME column of student_detail from VARCHAR (25) to VARCHAR (35).
+alter table STUDENT_DETAIL
+ALTER COLUMN Name varchar(35);
+
+--3. Change the data type DECIMAL to INT in CPI Column.
+alter table STUDENT_DETAIL
+ALTER COLUMN CPI INT;
+
+--4. Rename Column Enrollment_No to ENO.
+sp_rename 'STUDENT_DETAIL.Enrollment_No',ENO;
+
+--5. Delete Column City from the student_detail table.
+alter tabel STUDENT_DETAIL
+DROP COLUMN CITY
+--6. Change name of table student_detail to STUDENT_MASTER
+SP_RENAM STUDENT_DETAIL,STUDENT_MASTER;
+--PART-A DELETE,TRUNCATE,DROP----------------------------
+
+--1. Delete all the records of DEPOSIT_DETAIL table having amount greater than and equals to 4000. 
+
+	delete from  DEPOSITE_DETAIL
+	WHERE AMOUNT>=4000
+
+	--2. Delete all the accounts CHANDI BRANCH.
+
+	delete from  DEPOSITE_DETAIL
+	WHERE BNAME ='CHANDI'
+
+	--3. Delete all the accounts having account number (ANO) is greater than 105.
+
+	delete from  DEPOSITE_DETAIL
+	where ANO>105
+
+	--4. Delete all the records of Deposit_Detail table. (Use Truncate)
+
+	truncate table DEPOSITE_DETAIL
+
+	--5. Remove Deposit_Detail table. (Use Drop)
+	DROP TABLE DEPOSITE_DETAIL
+
