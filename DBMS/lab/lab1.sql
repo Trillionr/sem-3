31735e71d49d935c33f099f58966c38c9c64088f
Bhimani Maulik <140812702+gituhihi@users.noreply.github.com>
2024-07-12 09:18:11 +0530

    Quaris.sql

--- DBMS/lab/lab1.sql
+++ DBMS/lab/lab1.sql
@@ -418,7 +418,7 @@ JoiningDate DATETIME,
 Salary DECIMAL (8,2),
 City VARCHAR(20)
 )
-insert into EMPLOYEE_MASTER value
+insert into EMPLOYEE_MASTER values
 (101,'Keyur','5-1-02',12000.00,'Rajkot'),
 (102,'Hardik','15-2-04',14000.00,'Ahmedabad'),
 (103,'Kajal','14-3-06' ,15000.00,'Baroda'),
@@ -427,3 +427,138 @@ insert into EMPLOYEE_MASTER value
 (106,'Mitesh','25-9-01' ,5000.00,'Jamnagar'),
 (107,'Meera',Null, 7000.00,'Morbi'),
 (108,'Kishan','6-2-03' ,10000.00,NULL)
+
+--Lab-5------------------------------------------------------
+create table student(
+StuID INT,
+FirstName VARCHAR(25),
+LastName VARCHAR(25),
+Website VARCHAR(50),
+City VARCHAR(25),
+Address VARCHAR(100)
+)
+
+INSERT INTO STUDENT VALUES
+(1011,'Keyur','Patel','techonthenet.com','Rajkot','A-303,''VASANT KUNJ'',RAJKOT'),
+(1022,'Hardik','Shah','digminecraft.com','Ahmedabad','''Ram Krupa'', Raiya Road'),
+(1033,'Kajal','Trivedi','bigactivities.com','Baroda','Raj bhavan plot, near garden'), 
+(1044,'Bhoomi','Gajera','checkyourmath.com','Ahmedabad','''Jigs Home'',Narol'), 
+(1055,'Harmit','Mitel','@me.darshan.com','Rajkot','B-55, Raj Residency'),
+(1066,'Ashok','Jani',NULL,'Baroda','A502, Club House Building')
+
+TRUNCATE TABLE student
+SELECT *FROM student
+
+--1. Display the name of students whose name starts with ‘k’.
+select FirstName
+from student
+where FirstName like 'K%';
+
+--2. Display the name of students whose name consists of five characters.
+SELECT *FROM student
+select FirstName
+from student
+where FirstName like '_____';
+--3. Retrieve the first name & last name of students whose city name ends with a & contains six characters.
+SELECT *FROM student
+select city,FirstName,LastName
+from student
+where city like'_____a';
+
+--4. Display all the students whose last name ends with ‘tel’.
+SELECT *FROM student
+select LastName
+from student
+where LastName like'%tel';
+--5. Display all the students whose first name starts with ‘ha’ & ends with‘t’.
+SELECT *FROM student
+select FirstName
+from student
+where FirstName like'Ha%t'
+
+--6. Display all the students whose first name starts with ‘k’ and third character is ‘y’.
+SELECT *FROM student
+select FirstName
+from student
+where FirstName like'K_y%';
+--7. Display the name of students having no website and name consists of five characters.
+SELECT *FROM student
+select Website
+from student
+where Website like '_____' or Website is null ;
+--8. Display all the students whose last name consist of ‘jer’. 
+select LastName
+from student
+where LastName like '%jer%';
+--9. Display all the students whose city name starts with either ‘r’ or ‘b’.
+select city
+from student
+where city like 'R%' or city like 'B%';
+
+--10. Display all the name students having websites.
+select *from student
+select Website
+from student
+where Website is not null
+--11. Display all the students whose name starts from alphabet A to H.
+select FirstName
+from student
+where FirstName like '[A-H]%';
+--12. Display all the students whose name’s second character is vowel.
+select FirstName
+from student
+where FirstName like '_[a,e,i,o,u]%'
+--13. Display the name of students having no website and name consists of minimum five characters.
+select FirstName,Website
+from student
+where FirstName like '_____' and Website is null
+--14. Display all the students whose last name starts with ‘Pat’. 
+select LastName
+from student
+where LastName like 'Pat%'
+--15. Display all the students whose city name does not starts with ‘b’.
+select City
+from student
+where City not like 'B%'
+
+--Part – B:
+--1. Display all the students whose name starts from alphabet A or H.
+select FirstName
+from student
+where FirstName like '[A-H]%';
+--2. Display all the students whose name’s second character is vowel and of and start with H.
+select FirstName
+from student
+where FirstName like 'H[a,e,i,o,u]%';
+--3. Display all the students whose last name does not ends with ‘a’.
+select LastName
+from student
+where LastName not like '%a';
+--4.Display all the students whose first name starts with consonant.
+select FirstName
+from student
+where FirstName NOT like '[A,E,I,O,U]%';
+--5. Display all the students whose website contains .net
+SELECT *FROM student
+select Website
+from student
+where Website like '%.net';
+
+--Part – C:
+--1. Display all the students whose address consist of -.
+select Address
+from student
+where Address like '%-%'
+--2. Display all the students whose address contains single quote or double quote.
+select Address
+from student
+where Address like '%''%'
+--3. Display all the students whose website contains @.
+select Website
+from student
+where Website like '%@%'
+
+--4. Display all the names those are either four or five characters.
+select FirstName
+from student
+where FirstName like '_____' or FirstName like'____';
