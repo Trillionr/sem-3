7bde70753f89d41d3033945751c0e4f9b0264811
Bhimani Maulik <140812702+gituhihi@users.noreply.github.com>
2024-07-16 21:21:17 +0530

    Create Recognize.java

---
+++ DS/lab/4-19-2024/Recognize.java
@@ -0,0 +1,45 @@
+
+import java.util.*;
+
+public class Recognize {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String :::");
+        String str = sc.nextLine();
+        if(CheckString(str)){
+            System.out.println("Valid String");
+        }        
+        else{
+            System.out.println("Invalid String");
+        }
+    }
+    public static boolean CheckString(String str) {
+        Stack<Character> stack = new Stack<Character>();
+        int index = 0;
+        stack.push('c');
+        char next = str.charAt(index);
+        while (next != 'c') {
+            if (next == ' ') {
+                return false;
+            }
+            stack.push(next);
+            index++;
+            next = str.charAt(index);
+        }
+        index++;
+        while (stack.peek() != 'c' && index < str.length()) {
+            next = str.charAt(index);
+            if (next != stack.pop()) {
+                return false;
+            }
+            index++;
+        }
+        next = str.charAt(index - 1);
+        if (next != ' ' && stack.peek() != 'c') {
+            return false;
+        } else {
+            return true;
+        }
+    
+    }
+}
